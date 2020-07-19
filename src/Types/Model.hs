6964e0d860a8ffd3ed87db0a7b28583d494346a7
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-18 22:07:54 -0600

    add BSON instances

--- src/Types/Model.hs
+++ src/Types/Model.hs
@@ -29,6 +29,7 @@ import           Control.Monad.Trans.Class
 import           Control.PseudoInverseCategory (EndoIso, piiso)
 import           Control.ShpadoinkleContinuation
 import           Data.Aeson
+import qualified Data.Bson as B
 import           Data.ByteString.Lazy (fromStrict, toStrict)
 import           Data.Either.Extra (eitherToMaybe)
 import           Data.Function ((&))
@@ -40,6 +41,7 @@ import qualified Data.Set as S
 import           Data.Text (Text, intercalate, split, reverse)
 import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import           Data.Time.Calendar
+import           Data.Tuple.Extra (uncurry3)
 import qualified Data.UUID as U
 import           Data.UUID.Next (nextUUID)
 import           GHC.Generics
@@ -231,6 +233,29 @@ nextThreadId z (ThreadId i) =
     Nothing -> i'
 
 
+data Gregorian = Gregorian { gregYear :: Integer, gregMonth :: Int, gregDay :: Int }
+  deriving (Eq, Show)
+
+
+gregToDay :: Gregorian -> Maybe Day
+gregToDay g = fromGregorianValid (gregYear g) (gregMonth g) (gregDay g)
+
+
+dayToGreg :: Day -> Gregorian
+dayToGreg = uncurry3 Gregorian . toGregorian
+
+
+instance B.Val Gregorian where
+  val g = B.Doc
+          [ "year" B.:= B.val (gregYear g)
+          , "month" B.:= B.val (gregMonth g)
+          , "day" B.:= B.val (gregDay g) ]
+  cast' (B.Doc d) = Gregorian <$> (d B.!? "year" >>= B.cast')
+                              <*> (d B.!? "month" >>= B.cast')
+                              <*> (d B.!? "day" >>= B.cast')
+  cast' _ = Nothing
+
+
 instance Ord RelationLabel where
   compare (SymmL x) (SymmL y) = compare x y
   compare (AsymL x) (AsymL y) = compare x y
@@ -245,6 +270,8 @@ instance Ord Relation where
   compare (Asym _) (Symm _) = GT
 
 
+deriving instance Typeable (Relation' Symmetric)
+deriving instance Typeable (Relation' Asymmetric)
 deriving instance Eq (Relation' Symmetric)
 deriving instance Eq (Relation' Asymmetric)
 deriving instance Ord (Relation' Symmetric)
@@ -279,6 +306,11 @@ instance ToJSON ThreadId where
   toJSON = String . U.toText . unThreadId
 
 
+instance B.Val ThreadId where
+  val = B.String . U.toText . unThreadId
+  cast' v = ThreadId <$> (B.cast' v >>= U.fromText)
+
+
 instance FromHttpApiData CategoryId where
   parseUrlPiece = maybe (Left "CategoryId: expected a UUID") (Right . CategoryId) . U.fromText
 
@@ -294,6 +326,11 @@ instance FromJSON CategoryId where
 
 instance ToJSON CategoryId where
   toJSON = String . U.toText . unCategoryId
+
+
+instance B.Val CategoryId where
+  val = B.String . U.toText . unCategoryId
+  cast' v = CategoryId <$> (B.cast' v >>= U.fromText)
 
 
 instance FromHttpApiData CommentId where
@@ -313,6 +350,11 @@ instance ToJSON CommentId where
   toJSON = String . U.toText . unCommentId
 
 
+instance B.Val CommentId where
+  val = B.String . U.toText . unCommentId
+  cast' v = CommentId <$> (B.cast' v >>= U.fromText)
+
+
 instance FromJSON UserId where
   parseJSON (String s) = pure (UserId s)
   parseJSON _ = fail "UserId: Expected String"
@@ -320,6 +362,11 @@ instance FromJSON UserId where
 
 instance ToJSON UserId where
   toJSON = String . unUserId
+
+
+instance B.Val UserId where
+  val = B.String . unUserId
+  cast' v = UserId <$> B.cast' v
 
 
 instance FromHttpApiData UserId where
@@ -339,6 +386,11 @@ instance ToJSON SessionId where
   toJSON = String . U.toText . unSessionId
 
 
+instance B.Val SessionId where
+  val = B.String . U.toText . unSessionId
+  cast' v = SessionId <$> (B.cast' v >>= U.fromText)
+
+
 instance FromHttpApiData SessionId where
   parseUrlPiece = maybe (Left "SessionId: expected a UUID") (Right . SessionId) . U.fromText
 
@@ -353,6 +405,11 @@ instance MimeRender OctetStream PasswordHash where
 
 instance MimeUnrender OctetStream PasswordHash where
   mimeUnrender _ = Right . PasswordHash . decodeUtf8 . toStrict
+
+
+instance B.Val PasswordHash where
+  val = B.String . unPasswordHash
+  cast' v = PasswordHash <$> B.cast' v
 
 
 instance FromJSON Category where
@@ -371,6 +428,23 @@ instance ToJSON Category where
                     , "threads" .= categoryThreadIds c
                     , "from" .= categoryCreatedFrom c
                     , "isTrash" .= categoryIsTrash c ]
+
+
+instance B.Val Category where
+  val x = B.Doc
+          [ "title" B.:= B.val (categoryTitle x)
+          , "id" B.:= B.val (categoryId x)
+          , "threads" B.:= B.val (categoryThreadIds x)
+          , "from" B.:= B.val (categoryCreatedFrom x)
+          , "isTrash" B.:= B.val (categoryIsTrash x) ]
+  cast' (B.Doc d) = do
+    t  <- B.lookup "title" d
+    i  <- B.lookup "id" d
+    ts <- B.lookup "threads" d
+    f  <- B.lookup "from" d
+    iT <- B.lookup "isTrash" d
+    return (Category t i ts f iT)
+  cast' _ = Nothing
 
 
 instance FromJSON Thread where
@@ -392,7 +466,27 @@ instance ToJSON Thread where
     , "author" .= threadAuthor t
     , "created" .= threadCreated t
     , "comments" .= threadCommentIds t
-    , "categorization" .= categorization t ]
+    , "categorization" .= categorization t
+    , "from" .= threadCreatedFrom t ]
+
+
+instance B.Val Thread where
+  val t = B.Doc
+          [ "id" B.:= B.val (threadId t)
+          , "title" B.:= B.val (threadTitle t)
+          , "author" B.:= B.val (threadAuthor t)
+          , "created" B.:= B.val (dayToGreg (threadCreated t))
+          , "comments" B.:= B.val (threadCommentIds t)
+          , "categorization" B.:= B.val (categorization t)
+          , "from" B.:= B.val (threadCreatedFrom t) ]
+  cast' (B.Doc d) = Thread <$> (d B.!? "id" >>= B.cast')
+                           <*> (d B.!? "title" >>= B.cast')
+                           <*> (d B.!? "author" >>= B.cast')
+                           <*> (d B.!? "created" >>= B.cast' >>= gregToDay)
+                           <*> (d B.!? "comments" >>= B.cast')
+                           <*> (d B.!? "categorization" >>= B.cast')
+                           <*> (d B.!? "from" >>= B.cast')
+  cast' _ = Nothing
 
 
 instance FromJSON Edit where
@@ -406,6 +500,14 @@ instance ToJSON Edit where
   toJSON e = object [ "created" .= editCreated e, "text" .= editText e ]
 
 
+instance B.Val Edit where
+  val e = B.Doc
+          [ "created" B.:= B.val (dayToGreg (editCreated e))
+          , "text" B.:= B.val (editText e) ]
+  cast' (B.Doc d) = Edit <$> (d B.!? "created" >>= B.cast' >>= gregToDay)
+                         <*> (d B.!? "text" >>= B.cast')
+
+
 instance FromJSON Comment where
   parseJSON = withObject "Comment" $ \o -> do
     i  <- o .: "id"
@@ -417,9 +519,23 @@ instance FromJSON Comment where
 
 instance ToJSON Comment where
   toJSON c = object
-    [ "author" .= commentAuthor c
+    [ "id" .= commentId c
+    , "author" .= commentAuthor c
     , "created" .= commentCreated c
     , "edits" .= commentEdits c ]
+
+
+instance B.Val Comment where
+  val c = B.Doc
+          [ "id" B.:= B.val (commentId c)
+          , "created" B.:= B.val (dayToGreg (commentCreated c))
+          , "author" B.:= B.val (commentAuthor c)
+          , "edits" B.:= B.val (commentEdits c) ]
+  cast' (B.Doc d) = Comment <$> (d B.!? "id" >>= B.cast')
+                            <*> (d B.!? "author" >>= B.cast')
+                            <*> (d B.!? "created" >>= B.cast' >>= gregToDay)
+                            <*> (d B.!? "edits" >>= B.cast')
+  cast' _ = Nothing
 
 
 instance FromJSON UserProfile where
@@ -439,6 +555,19 @@ instance ToJSON UserProfile where
     , "created" .= userCreated u ]
 
 
+instance B.Val UserProfile where
+  val u = B.Doc
+        [ "id" B.:= B.val (userId u)
+        , "fullName" B.:= B.val (userFullName u)
+        , "email" B.:= B.val (userEmail u)
+        , "created" B.:= B.val (dayToGreg (userCreated u)) ]
+  cast' (B.Doc d) = UserProfile <$> (d B.!? "id" >>= B.cast')
+                                <*> (d B.!? "fullName" >>= B.cast')
+                                <*> (d B.!? "email" >>= B.cast')
+                                <*> (d B.!? "created" >>= B.cast' >>= gregToDay)
+  cast' _ = Nothing
+
+
 --instance FromJSON Symmetry where
 --  parseJSON (String "symm") = pure Symmetric
 --  parseJSON (String "asym") = pure Asymmetric
@@ -452,6 +581,12 @@ instance FromJSON (RelationLabel' Symmetric) where
 
 instance ToJSON (RelationLabel' Symmetric) where
   toJSON (SymmL' a) = object [ "activeVoice" .= a ]
+
+
+instance B.Val (RelationLabel' Symmetric) where
+  val (SymmL' a) = B.Doc [ "activeVoice" B.:= B.String a ]
+  cast' (B.Doc d) = SymmL' <$> (d B.!? "activeVoice" >>= B.cast')
+  cast' _ = Nothing
 
 
 instance ToHttpApiData (RelationLabel' Symmetric) where
@@ -473,6 +608,14 @@ instance ToJSON (RelationLabel' Asymmetric) where
   toJSON (AsymL' a p) = object [ "activeVoice" .= a, "passiveVoice" .= p ]
 
 
+instance B.Val (RelationLabel' Asymmetric) where
+  val (AsymL' a p) = B.Doc
+                     [ "activeVoice" B.:= B.String a
+                     , "passiveVoice" B.:= B.String p ]
+  cast' (B.Doc d) = AsymL' <$> (d B.!? "activeVoice" >>= B.cast')
+                           <*> (d B.!? "passiveVoice" >>= B.cast')
+
+
 instance ToHttpApiData (RelationLabel' Asymmetric) where
   toUrlPiece (AsymL' a p) = a <> "," <> p
 
@@ -490,9 +633,12 @@ instance FromJSON RelationLabel where
 
 
 instance ToJSON RelationLabel where
-  toJSON = unRelationLabel unSymmetricLabel unAsymmetricLabel
-    where unSymmetricLabel = object . (:[]) . ("activeVoice" .=)
-          unAsymmetricLabel a p = object [ "activeVoice" .= a, "passiveVoice" .= p ]
+  toJSON = unRelationLabel (toJSON . SymmL') (\a p -> toJSON (AsymL' a p))
+
+
+instance B.Val RelationLabel where
+  val = unRelationLabel (B.val . SymmL') (\a p -> B.val (AsymL' a p))
+  cast' v = (SymmL <$> B.cast' v) <|> (AsymL <$> B.cast' v)
 
 
 instance FromHttpApiData RelationLabel where
@@ -519,6 +665,21 @@ instance ToJSON (RelationLabel' s) => ToJSON (Relation' s) where
                                  , "predicateObject" .= p ]
 
 
+instance ( Typeable (Relation' s)
+         , Eq (Relation' s)
+         , Show (Relation' s)
+         , B.Val (RelationLabel' s) )
+         => B.Val (Relation' s) where
+  val (Rel l (s, p)) = B.Doc
+                       [ "label" B.:= B.val l
+                       , "subject" B.:= B.val s
+                       , "predicateObject" B.:= B.val p ]
+  cast' (B.Doc d) = Rel <$> (d B.!? "label" >>= B.cast')
+                <*> ((,) <$> (d B.!? "subject" >>= B.cast')
+                         <*> (d B.!? "predicateObject" >>= B.cast'))
+  cast' _ = Nothing
+
+
 instance FromHttpApiData (RelationLabel' s) => FromHttpApiData (Relation' s) where
   parseUrlPiece x = case split (== ':') x of
     (l:s:p:_) -> do
@@ -540,6 +701,12 @@ instance FromJSON Relation where
 instance ToJSON Relation where
   toJSON (Symm r) = toJSON r
   toJSON (Asym r) = toJSON r
+
+
+instance B.Val Relation where
+  val (Symm r) = B.val r
+  val (Asym r) = B.val r
+  cast' v = (Asym <$> B.cast' v) <|> (Symm <$> B.cast' v)
 
 
 instance FromHttpApiData Relation where
@@ -565,6 +732,17 @@ instance ToJSON Session where
     [ "id" .= sessionId s
     , "user" .= sessionUser s
     , "created" .= sessionCreated s]
+
+
+instance B.Val Session where
+  val s = B.Doc
+          [ "id" B.:= B.val (sessionId s)
+          , "user" B.:= B.val (sessionUser s)
+          , "created" B.:= B.val (dayToGreg (sessionCreated s)) ]
+  cast' (B.Doc d) = Session
+                <$> (d B.!? "id" >>= B.cast') 
+                <*> (d B.!? "user" >>= B.cast')
+                <*> (d B.!? "created" >>= B.cast' >>= gregToDay)
 
 
 mapBy :: Ord k => (v -> k) -> [v] -> M.Map k v
@@ -606,3 +784,26 @@ instance ToJSON Zettel where
              , "users" .= M.elems (users z)
              , "session" .= session z ]
 
+
+instance B.Val Zettel where
+  val z = B.Doc
+          [ "categories" B.:= B.val (M.elems (categories z))
+          , "categoryOrdering" B.:= B.val (categoryOrdering z)
+          , "threads" B.:= B.val (M.elems (threads z))
+          , "trashcan" B.:= B.val (S.toList (trashcan z))
+          , "comments" B.:= B.val (M.elems (comments z))
+          , "relationLabels" B.:= B.val (S.toList (relationLabels z))
+          , "relations" B.:= B.val (S.toList (relations z))
+          , "users" B.:= B.val (M.elems (users z))
+          , "session" B.:= B.val (session z) ]
+  cast' (B.Doc d) = Zettel
+                <$> (mapBy categoryId <$> (d B.!? "categories" >>= B.cast'))
+                <*> (d B.!? "categoryOrdering" >>= B.cast')
+                <*> (mapBy threadId <$> (d B.!? "threads" >>= B.cast'))
+                <*> (S.fromList <$> (d B.!? "trashcan" >>= B.cast'))
+                <*> (mapBy commentId <$> (d B.!? "comments" >>= B.cast'))
+                <*> (S.fromList <$> (d B.!? "relationLabels" >>= B.cast'))
+                <*> (S.fromList <$> (d B.!? "relations" >>= B.cast'))
+                <*> (mapBy userId <$> (d B.!? "users" >>= B.cast'))
+                <*> (d B.!? "session" >>= B.cast')
+  cast' _ = Nothing
